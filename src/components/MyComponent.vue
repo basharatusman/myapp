322861df9e8cc0f8e6f08b5f44032b5fe0c098bb
basharatusman.a <basharatusman.a@gmail.com>
2023-12-19 20:23:28 +0000

    initial commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>This is my component</h1>
+      <h2>{{ title }}</h2>
+    </div>
+  </template>
+  
+  <script lang ="ts" setup>
+  import { ref } from 'vue';
+  
+  const title = ref('Hello, world!');
+
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: red;
+  }
+  </style>
+  
